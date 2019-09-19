9cd6ff78453d51258ec700c0bbc8a6682309d723
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-09-19 19:15:37 +0000

    I sometimes suffer from insomnia commit!

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 97; c <= 122; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
