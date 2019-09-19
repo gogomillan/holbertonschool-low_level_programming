cecd1b4502a18631511195c44e84d4c96a105bbe
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-09-19 19:31:41 +0000

    Numbers commit!

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+		printf("%d", c);
+	printf("\n");
+
+	return (0);
+}
