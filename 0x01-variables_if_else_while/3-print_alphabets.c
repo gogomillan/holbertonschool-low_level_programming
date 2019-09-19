22833e225eb02f4222a779a846d1dcc242c2559e
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-09-19 19:18:59 +0000

    alphABET commit!

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 97; c <= 122; c++)
+		putchar(c);
+	for (c = 65; c <= 90; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
