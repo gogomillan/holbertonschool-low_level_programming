9d1d54519ae709dbaeb4437bf6da94816cf41440
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-09-24 21:53:37 +0000

    Fibonacci 100 commit!

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Compute and print the first 50 Fibonacci numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long double i, a, b, t;
+
+	a = 1;
+	b = 2;
+	printf("1");
+	for (i = 1; i < 97; i++)
+	{
+		printf(", %0.Lf", b);
+		t = a;
+		a = b;
+		b = t + b;
+	}
+
+	printf("\n");
+	return (0);
+}
