40a022d1c2ac0db14eb492e3abf14fe2eba97685
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-09-24 18:05:11 +0000

    Holberton School 98 commit!

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,11 +12,8 @@ void print_to_98(int n)
 	int i;
 
 	for (i = n; i <= 98; i++)
-	{
-		printf("%d", i);
-		if (i != 98)
-			printf(", ");
-	}
-	if (n <= 98)
-		printf("\n");
+		printf("%d%s", i, (i != 98 ? ", " : ""));
+	for (i = n; i >= 98; i--)
+		printf("%d%s", i, (i != 98 ? ", " : ""));
+	printf("\n");
 }
