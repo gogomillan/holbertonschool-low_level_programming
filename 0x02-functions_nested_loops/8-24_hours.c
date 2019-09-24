988e6c4cd9658adb969efa7e62c63342d91bbf99
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-09-24 14:31:21 +0000

    Today is the longest day of my life commit!

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - This function print every minute of the day
+ *
+ * Return: Nothing
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 23; i++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			_putchar(48 + i / 10);
+			_putchar(48 + i % 10);
+			_putchar(':');
+			_putchar(48 + j / 10);
+			_putchar(48 + j % 10);
+			_putchar('\n');
+		}
+	}
+}
