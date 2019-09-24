29cdd7c79f78083e08db79efaffe57ba174dafa2
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-09-24 13:43:06 +0000

    -abs function commit!

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+/**
+ * _abs - This function calculate the abs value
+ * @n: The value to evaluate
+ *
+ * Return: 1 if n is greater than zero, 0 is zero and -1 if n is less than zero
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	return (n * -1);
+}
