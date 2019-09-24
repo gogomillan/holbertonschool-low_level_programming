0b113cf0c4447d1ba80c656619ad14ddba2ed07b
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-09-24 17:13:46 +0000

    Holberton School 98 commit!

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - This is the controller
+ * @n: Starting point
+ *
+ * Return: Nothing
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		printf("%d", i);
+		if (i != 98)
+			printf(", ");
+	}
+	if (n <= 98)
+		printf("\n");
+}
