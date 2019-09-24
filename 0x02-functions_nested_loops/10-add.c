438b91f5a75faa6671586823c75294c44e750555
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-09-24 16:44:38 +0000

    a + b commit!

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+ * add - This function sum two values
+ * @a: The first value to evaluate
+ * @b: The second value to evaluate
+ *
+ * Return: the sum of a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
