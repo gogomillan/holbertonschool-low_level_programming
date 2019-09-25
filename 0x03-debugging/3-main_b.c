7ff4d9b163459d621c013ab5ae1963cc8e87b95c
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-09-25 04:16:11 +0000

    Leap year fix04 commit!

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -23,5 +23,45 @@ int main(void)
 
 	print_remaining_days(month, day, year);
 
+	month = 2;
+	day = 29;
+	year = 1991;
+
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+	day = convert_day(month, day);
+
+	print_remaining_days(month, day, year);
+
+	month = 2;
+	day = 29;
+	year = 2047;
+
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+	day = convert_day(month, day);
+
+	print_remaining_days(month, day, year);
+
+	month = 3;
+	day = 01;
+	year = 2000;
+
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+	day = convert_day(month, day);
+
+	print_remaining_days(month, day, year);
+
+	month = 4;
+	day = 01;
+	year = 2000;
+
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+	day = convert_day(month, day);
+
+	print_remaining_days(month, day, year);
+
 	return (0);
 }
