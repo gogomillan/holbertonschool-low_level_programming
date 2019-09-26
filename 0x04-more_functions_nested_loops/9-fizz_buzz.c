3a4c2eed1d01dd3ce92fc1317d02f94100ac4db1
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-09-26 19:57:25 +0000

    Fizz-Buzz fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,14 +10,17 @@ int main(void)
 	int i;
 
 	for (i = 1; i <= 100; i++)
+	{
 		if ((i % 5) == 0 && (i % 3) == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if ((i % 5) == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if ((i % 3) == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else
-			printf("%d ", i);
+			printf("%d", i);
+		printf("%s", i < 100 ? " " : "");
+	}
 	printf("\n");
 
 	return (0);
