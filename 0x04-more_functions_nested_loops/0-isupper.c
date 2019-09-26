be8b8f92306fec016683f4535af314d22e07a244
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-09-26 14:58:41 +0000

    _isupper commit!

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <ctype.h>
+
+/**
+ * _isupper - Checks for an uppercase letter
+ * @c: The int value of character to evaluate
+ *
+ * Return: 1 if uppercase, other case 0.
+ */
+int _isupper(int c)
+{
+	if (isupper(c) != 0)
+		return (1);
+	return (0);
+}
