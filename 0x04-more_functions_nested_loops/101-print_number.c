09ad7a02dbafe66f7e17adca39f3c181dd8973a0
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-09-27 02:42:17 +0000

    Numbers have life commit!

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,21 +12,21 @@ void print_number(int n)
 	char o = 48;
 	int  i = n;
 
-	if ((n / 10) == 0)
+	if (n / 10 == 0)
 	{
 		if (n < 0)
 		{
 			_putchar(h);
 			i *= -1;
 		}
-		o = o + i;
-		_putchar(o);
+		_putchar(o + i);
 		return;
 	}
 
 	print_number(n / 10);
 	if (n < 0)
 		i *= -1;
+
 	o = o + (i % 10);
 	_putchar(o);
 }
