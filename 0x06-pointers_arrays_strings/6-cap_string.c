c0145b2d846d843b8729c3de1ef3eac55dd78eb1
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-10-04 00:25:51 +0000

    Expect the best. commit!

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * cap_string - Change to Capitalize
+ * @s: The array
+ *
+ * Return: none
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+	char t[] = { ' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\0' };
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		j = 0;
+		while (t[j] != '\0')
+		{
+			if (s[i] == t[j])
+				if (s[i+1] >= 'a' && s[i+1] <= 'z')
+					s[i+1] = s[i+1] - 32;
+			j++;
+		}
+	}
+
+	return (s);
+}
