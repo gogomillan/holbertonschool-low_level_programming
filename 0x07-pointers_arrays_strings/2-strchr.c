aea7014e7662aed66b44689caa3bd2d50189e451
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-10-07 17:22:47 +0000

    strspn commit!

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a character in a string.
+ * @s: the buffer source
+ * @c: the char to find
+ *
+ * Return: The pointer to first occurency or NULL.
+ */
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; s++)
+		if (*s == c)
+			return (s);
+
+	return (NULL);
+}
