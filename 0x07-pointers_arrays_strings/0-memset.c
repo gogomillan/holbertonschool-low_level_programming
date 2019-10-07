94171f6402ba4bd719052ba5d0c05313e44f012d
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-10-07 15:23:35 +0000

    memset commit!

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _memset - fills memory with a constant type
+ * @s: the buffer
+ * @b: the constant
+ * @n: the size
+ *
+ * Return: Nothing.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) = b;
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /* HOLBERTON_H */
