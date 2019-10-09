816fbb2709ff2b9d85e3812bf56fdbc34c21cdaa
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-10-09 17:48:48 +0000

    She locked away a secret

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif /* HOLBERTON_H */
