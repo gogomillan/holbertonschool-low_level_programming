9de7f16f1e70cfdb5861139a5f95aa67ce428dc3
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-10-11 05:56:50 +0000

    Inception

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,8 +1,12 @@
 #include "holberton.h"
 
+char *_strchr(char *s, char c);
+char *_strstr(char *haystack, char *needle);
+
 /**
  * wildcmp - Compares two string to find out if they are identical
- * @s: The string
+ * @s1: The string 1
+ * @s2: The string 2
  *
  * Return: 1 if true or 0 if false
  */
@@ -14,10 +18,72 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(++s1, ++s2));
 	else if (*s2 == '*' && *(s2 + 1) == '*')
 		return (wildcmp(s1, ++s2));
-	else if (*s2 == '*' && *s1 != *(s2 + 1))
-		return (wildcmp(++s1, s2));
-	else if (*s2 == '*' && *s1 == *(s2 + 1))
-		return (wildcmp(s1, ++s2));
+	else if (*s2 == '*' && *(s2 + 1) == '\0')
+		if (*s1 != '\0')
+			return (wildcmp(++s1, s2));
+		else
+			return (wildcmp(s1, ++s2));
+	else if (*s2 == '*' && *(s2 + 1) != '*')
+	{
+		if (_strstr(s1, (s2 + 1)) != NULL)
+			return (wildcmp(_strstr(s1, s2 + 1), s2 + 1));
+		else if (_strchr(s1, *(s2 + 1)) != NULL)
+			return (wildcmp(_strchr(s1, *(s2 + 1)), s2 + 1));
+		s2++;
+		return (wildcmp(s1, s2));
+	}
 
 	return (0);
+}
+
+/**
+ * _strchr - locates a character in a string.
+ * @s: the buffer source
+ * @c: the char to find
+ *
+ * Return: The pointer to first occurency or NULL.
+ */
+char *_strchr(char *s, char c)
+{
+int i = 0, j;
+
+	while (*(s + i) != '\0')
+		i++;
+	for (j = 0; j <= i; j++, s++)
+		if (*s == c)
+			return (s);
+
+	return (NULL);
+}
+
+/**
+ * _strstr - locates a substring.
+ * @haystack: the buffer source
+ * @needle: the string to find
+ *
+ * Return: The quantity of bytes contents
+ */
+char *_strstr(char *haystack, char *needle)
+{
+unsigned int i = 0, j;
+char *h = haystack, *n = needle;
+
+	while (needle[i] != '\0')
+		i++;
+
+	while (*haystack != '\0')
+	{
+		h = haystack;
+		n = needle;
+		for (j = 0; j <= i && *h == *n && *h != '\0' && *n != '\0'; j++)
+		{
+			h++;
+			n++;
+		}
+		if ((j - i) == 0)
+			return (haystack);
+		haystack++;
+	}
+
+	return (NULL);
 }
