c555a87c441cdd796ebedf89a36c46b533a20d0d
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-10-13 02:49:46 +0000

    Silence is argument

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -12,12 +12,10 @@ int main(int argc, char **argv)
 	if (**argv == 0 || argc < 1)
 		return (1);
 	argc--;
-	do
-	{
+	do {
 		_putchar((argc % 10) + 48);
 		argc = argc / 10;
-	}
-	while (argc > 0);
+	} while (argc > 0);
 	_putchar('\n');
 
 	return (0);
