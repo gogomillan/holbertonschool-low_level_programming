ac2325174667d4b32c659c0d72f5a1eb571ebf83
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-10-13 17:06:26 +0000

    Minimal number

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: Qty of parameters
+ * @argv: The parameters themselves
+ *
+ * Return: 1 if no parameters, 0 if parameter are OK.
+ */
+int main(int argc, char **argv)
+{
+int  a = 0, q = 0, c[] = { 25, 10, 5, 2, 1, 0 }, *cp = c;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if ((a = atoi(argv[1])) < 0)
+		q = 0;
+	while (*cp)
+	{
+		q += a / *cp;
+		a -= (a / *cp) * *cp;
+		cp++;
+	}
+	printf("%d\n", q);
+
+	return (0);
+}
