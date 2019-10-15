772522c4e708c3563322edf858db85dff6904569
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-10-15 20:13:59 +0000

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strdup - create a new same string from other
+ * @str: the original string
+ *
+ * Return: The new space of memory to the new string.
+ */
+char *_strdup(char *str)
+{
+unsigned int j = 0, size;
+char *s;
+
+	while (str[j] != '\0')
+		j++;
+	size = j + 1;
+	s = (char *) malloc(size * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+
+	for (j = 0; j < size; j++)
+		s[j] = str[j];
+	return (s);
+}
