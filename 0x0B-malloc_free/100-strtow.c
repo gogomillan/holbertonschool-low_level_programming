66ea5eff7403ff81317589425d837b3abc6c8159
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-10-16 07:32:14 +0000

    I will show you how great I am

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,78 @@
+#include "holberton.h"
+
+void _countword(char *str, int *t, int *lm);
+
+/**
+ * strtow - Split a string into words
+ * @str: the string
+ *
+ * Return: A pointer to the array of words.
+ */
+char **strtow(char *str)
+{
+char **p;
+int i, j, t = 0, l = 0, lm = 0, sp = 0;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	_countword(str, &t, &lm);
+	p = (char **) malloc((t + 1) * sizeof(char *));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i <= t; i++)
+		*(p + i) = (char *) malloc((lm + 1) * sizeof(char));
+		if (*(p + i) == NULL)
+			return (NULL);
+	i = l = 0;
+	sp = 1;
+	for (j = 0; str[j] != '\0'; j++)
+		if (str[j] == ' ')
+		{
+			if (sp == 0)
+			{
+				p[i][l] = '\0';
+				l = 0;
+				i++;
+			}
+			sp = 1;
+		}
+		else
+		{
+			p[i][l] = str[j];
+			l++;
+			sp = 0;
+		}
+	if (sp == 0)
+	{
+		p[i][l] = '\0';
+		i++;
+	}
+	p[i] = NULL;
+	return (p);
+}
+
+void _countword(char *str, int *t, int *lm)
+{
+int i, l = 0, sp = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		if (str[i] == ' ')
+		{
+			if (sp == 0)
+			{
+				(*t)++;
+				if (l > *lm)
+					*lm = l;
+				l = 0;
+			}
+			sp = 1;
+		}
+		else
+		{
+			l++;
+			sp = 0;
+		}
+	if (sp == 0)
+		(*t)++;
+}
