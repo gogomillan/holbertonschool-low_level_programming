f871f6741b45bccbfa516f7ab662afa6d292bd7e
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-10-17 20:06:07 +0000

    _calloc mine

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _calloc - reserve a space in memory of size lenght
+ * @nmemb: Quantity of elements
+ * @size: The size of each element
+ *
+ * Return: NULL if error or the pointer.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *p;
+unsigned int b, i;
+
+	b = nmemb * size;
+	p = malloc(b);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		p[i] = 0;
+	return (p);
+}
