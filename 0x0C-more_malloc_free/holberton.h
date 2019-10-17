ca33e4121edf112be639b1a8cd4d552732a91989
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-10-17 20:57:19 +0000

    _calloc mine

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,10 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <limits.h>
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
