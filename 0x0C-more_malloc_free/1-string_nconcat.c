eafae942b8b52bcc66c4204991078efc4f46f256
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-10-17 19:17:32 +0000

    string_nconcat commit!

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * string_nconcat - concatenate two string
+ * @s1: the first string
+ * @s2: the second string
+ * @n: the qty of second string to copy
+ *
+ * Return: A pointer to the new concatenated string.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int i = 0, j = 0, size;
+char *s;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+	if (j > n)
+		j = n;
+	size = i + j + 1;
+	s = (char *) malloc(size * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+
+	for (j = 0; j < size; j++)
+		if (*s1 != '\0')
+			s[j] = *(s1++);
+		else
+			s[j] = *(s2++);
+
+	return (s);
+}
