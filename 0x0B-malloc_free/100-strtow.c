239dc4405727ece474345a44b7afbc36f77ef747
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-10-17 21:25:43 +0000

    Update

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 
-void _countword(char *str, int *t, int *lm);
+void _countword(char *str, int *t);
 
 /**
  * strtow - Split a string into words
@@ -11,19 +11,19 @@ void _countword(char *str, int *t, int *lm);
 char **strtow(char *str)
 {
 char **p;
-int i, j, t = 0, l = 0, lm = 0, sp = 0;
+int i, j, t = 0, l = 0, sp = 0;
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
 
-	_countword(str, &t, &lm);
+	_countword(str, &t);
 	p = (char **) malloc((t + 1) * sizeof(char *));
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; i <= t; i++)
-		*(p + i) = (char *) malloc((lm + 1) * sizeof(char));
-		if (*(p + i) == NULL)
-			return (NULL);
+	//for (i = 0; i <= t; i++)
+		//*(p + i) = (char *) malloc((lm + 1) * sizeof(char));
+		//if (*(p + i) == NULL)
+			//return (NULL);
 	i = l = 0;
 	sp = 1;
 	for (j = 0; str[j] != '\0'; j++)
@@ -52,27 +52,19 @@ int i, j, t = 0, l = 0, lm = 0, sp = 0;
 	return (p);
 }
 
-void _countword(char *str, int *t, int *lm)
+void _countword(char *str, int *t)
 {
-int i, l = 0, sp = 0;
+int i, sp = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
 		if (str[i] == ' ')
 		{
 			if (sp == 0)
-			{
 				(*t)++;
-				if (l > *lm)
-					*lm = l;
-				l = 0;
-			}
 			sp = 1;
 		}
 		else
-		{
-			l++;
 			sp = 0;
-		}
 	if (sp == 0)
 		(*t)++;
 }
