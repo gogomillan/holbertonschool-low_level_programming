05c1b8403cdf0b3a16d4b63de99d5d924c0a2886
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-10-21 21:59:26 +0000

    Django

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+
+/**
+ * struct dog - Like a class for a dog
+ * @name: The name
+ * @age: The age
+ * @owner: The dog's owner
+ *
+ * Description: This scructure let define the characteristics of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* HOLBERTON_H */
