7977487894283a562014b6606e60b37a76143d87
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-10-21 19:47:27 +0000

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("%s\n", __FILE__);
+    return (0);
+}
