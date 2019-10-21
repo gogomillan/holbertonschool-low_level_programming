9f293d473d2448995bc466be028824f351e1f17d
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-10-21 22:51:39 +0000

    A dog print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,10 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name : %s\n", d->name == NULL ? "(nil)" : d->name);
-		printf("Name : %f\n", d->age);
+		if (d->age == 0)
+			printf("Name : (nil)\n");
+		else
+			printf("Name : %f\n", d->age);
 		printf("Name : %s\n", d->owner == NULL ? "(nil)" : d->owner);
 	}
 }
