20bb67184e1b60aa2395e2de12867842d1d8b007
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-10-22 00:15:22 +0000

    A door is what a dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,8 @@
 #include "dog.h"
 
+unsigned int _lenght(char *s);
+char *_strcpy(char *d, char *s);
+
 /**
  * new_dog - Create the structure
  * @name: The name
@@ -16,13 +19,13 @@ char  *n, *o;
 	my_dog = malloc(sizeof(dog_t));
 	if (my_dog == NULL)
 		return (NULL);
-	n = malloc(strlen(name));
+	n = malloc(_lenght(name));
 	if (n == NULL)
 	{
 		free(my_dog);
 		return (NULL);
 	}
-	o = malloc(strlen(name));
+	o = malloc(_lenght(name));
 	if (o == NULL)
 	{
 		free(my_dog);
@@ -30,11 +33,43 @@ char  *n, *o;
 		return (NULL);
 	}
 
-	strcpy(n, name);
-	strcpy(o, owner);
+	_strcpy(n, name);
+	_strcpy(o, owner);
 	my_dog->name = n;
 	my_dog->owner = o;
 	my_dog->age = age;
 
 	return (my_dog);
+}
+
+/**
+ * _lenght - Find out the lenght of a string
+ * @s: The string
+ *
+ * Return: Lenght.
+ */
+unsigned int _lenght(char *s)
+{
+unsigned long i = 0;
+
+    while (s[i] != '\0')
+        i++;
+    return (i);
+}
+
+/**
+ * _lenght - Find out the lenght of a string
+ * @s: The string
+ *
+ * Return: Lenght.
+ */
+char *_strcpy(char *d, char *s)
+{
+unsigned long i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		d[i] = s[i];
+	d[i] = s[i];
+
+    return (d);
 }
