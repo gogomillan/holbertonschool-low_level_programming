5f1a1f1345d48282b48a06518f1f14570265f6cd
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-10-23 18:58:40 +0000

    To tell with circumstances

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Search for an integer
+ * @array: Array of elements
+ * @size: The size
+ * @cmp: The name of the function
+ *
+ * Return: The index of first element matched or 0.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+		if ((*cmp)(array[i]) != 0)
+			return (i);
+	return (0);
+}
