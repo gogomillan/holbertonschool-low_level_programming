492efced67d4da2b2adf1f1ca82150cb5fb52679
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-10-23 17:12:25 +0000

    What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include "function_pointers.h"
+
 /**
  * print_name - prints a name
  * @name: name of the person
@@ -7,5 +9,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if (name != NULL)
+		(*f)(name);
 }
