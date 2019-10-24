20cfaff6c3668047354855aa1ce262a0e86512ff
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-10-24 05:26:36 +0000

    fix

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -37,12 +37,12 @@ int i;
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		exit (1);
 	}
 	if (atoi(argv[1]) < 0)
 	{
 		printf("Error\n");
-		return (2);
+		exit (2);
 	}
 
 	for (i = 0; i < atoi(argv[1]); i++)
