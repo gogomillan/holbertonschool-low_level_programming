f7d97d02caef07e8e5e8d151ef2c637a928313e5
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-10-24 01:12:45 +0000

    A goal is not always meant to be reached

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+
+/**
+ * main - operate two numbers
+ *
+ * @argc: The qty of parameters
+ * @argv: The parameters array
+ *
+ * Return: The add
+ *
+ */
+int main(int argc, char **argv)
+{
+char o = argv[2][0];
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return(98);
+	}
+	if (o != '+' && o != '-' && o != '*' && o != '/' && o != '%')
+	{
+		printf("Error\n");
+		return(99);
+	}
+	if ((o == '/' || o == '%') && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		return(100);
+	}
+
+	printf("[%d]\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,69 @@
+/**
+ * op_add - add two numbers
+ *
+ * @a: The first argument
+ * @b: The second argument
+ *
+ * Return: The add
+ *
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract two numbers
+ *
+ * @a: The first argument
+ * @b: The second argument
+ *
+ * Return: The substraction
+ *
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply two numbers
+ *
+ * @a: The first argument
+ * @b: The second argument
+ *
+ * Return: The product
+ *
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide two numbers
+ *
+ * @a: The first argument
+ * @b: The second argument
+ *
+ * Return: The cuotient
+ *
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - divide two numbers
+ *
+ * @a: The first argument
+ * @b: The second argument
+ *
+ * Return: The remaining
+ *
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
