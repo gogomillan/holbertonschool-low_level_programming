ef81c38756f72fe6f652f148e8a88163317e6c49
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-10-27 12:40:16 +0000

    Variadic functions

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,14 +1,15 @@
-section     .text
-	global  main
-main:
-	mov     eax,4
-	mov     ebx,1
-	mov     ecx,msg
-	mov     edx,len
-	int     0x80
-	mov     eax,1
-	int     0x80
+section .data
+	msg db 'Hello, Holberton', `\n`
+	len equ $ - msg
 
-section     .data
-	msg     db  'Hello, Holberton',0xa
-	len     equ $ - msg
+section .text
+	global main
+main:
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, msg
+	mov edx, len
+	int 0x80
+
+	mov eax, 1
+	int 0x80
