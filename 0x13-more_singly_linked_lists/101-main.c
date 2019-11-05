6f2ac69cc8cd74c81610ed9262b93c8a50fa4180
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-11-05 16:45:58 +0000

    Fix boundaries

--- 0x13-more_singly_linked_lists/101-main.c
+++ 0x13-more_singly_linked_lists/101-main.c
@@ -23,7 +23,7 @@ int main(void)
     add_nodeint(&head2, 98);
     add_nodeint(&head2, 402);
     add_nodeint(&head2, 1024);
-    print_listint_safe(head2);
+    printf("Tot: %d\n", (int)print_listint_safe(head2));
     head = NULL;
     node = add_nodeint(&head, 0);
     add_nodeint(&head, 1);
@@ -33,6 +33,6 @@ int main(void)
     node->next = add_nodeint(&head, 98);
     add_nodeint(&head, 402);
     add_nodeint(&head, 1024);
-    print_listint_safe(head);
+    printf("Tot: %d\n", (int)print_listint_safe(head));
     return (0);
 }
