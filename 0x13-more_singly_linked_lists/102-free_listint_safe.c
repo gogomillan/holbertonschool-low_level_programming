a01b4536958694c15b628c6c84f755ea06eddaa6
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-11-05 20:09:10 +0000

    Free (safe version)

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - Realease all the elements of a listint_t list.
+ * @h: Head of the list
+ *
+ * Return: Return the number of elements
+ */
+size_t free_listint_safe(listint_t **h)
+{
+size_t c = 0;
+listint_t *e, *prev;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	e = *h;
+	while (e != NULL)
+	{
+		prev = e;
+		c++;
+		if (e->next > e)
+		{
+			e = e->next;
+			free(prev);
+			free(e);
+			c++;
+			break;
+		}
+		e = e->next;
+		free(prev);
+	}
+	*h = NULL;
+
+	return (c);
+}
