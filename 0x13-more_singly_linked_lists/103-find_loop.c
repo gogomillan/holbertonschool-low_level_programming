ba7604eb3e50095d21e8d59f07e2fb6578334e47
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-11-05 21:22:26 +0000

    Find the loop

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - Find the loopin the elements of a listint_t list.
+ * @head: Head of the list
+ *
+ * Return: Return the element when the loop starts
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+listint_t *e = head;
+
+	while (e != NULL)
+	{
+		if (e->next > e)
+		{
+			e = e->next;
+			return (e);
+		}
+		e = e->next;
+	}
+
+	return (e);
+}
