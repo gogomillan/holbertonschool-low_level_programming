c1c6cd552e4b43f18792701d618876039fe4232d
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-11-05 15:43:41 +0000

    Printf (safe version) initial commit!

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - Prints all the elements of a listint_t list.
+ * @head: Head of the list
+ *
+ * Return: Return the number of elements
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+size_t c = 0;
+listint_t *e = (listint_t *)head;
+
+	while (e != NULL)
+	{
+		printf("[%p] %d\n", (void *)e, e->n);
+		c++;
+		if (e->next > e)
+		{
+			e = e->next;
+			printf("-> [%p] %d\n", (void *)e, e->n);
+			exit(98);
+		}
+		e = e->next;
+	}
+
+	return (c);
+}
