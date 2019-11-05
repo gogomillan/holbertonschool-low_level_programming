00832009a055c3c49c12c9465f08d07a8329b719
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-11-05 08:43:45 +0000

    Fix boundaries operations

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,34 +10,37 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 unsigned int c = 0;
-listint_t *node, *next;
+listint_t *node, *prev;
 
-	if (head == NULL)
-		return (-1);
-
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 
 	node = *head;
-	while (node != NULL && c < index)
+	prev = node;
+	while (node->next != NULL && c < index)
 	{
 		c++;
+		prev = node;
 		node = node->next;
 	}
 	if (c != index)
 		return (-1);
 
-	if (node->next != NULL)
+	if (node->next == NULL)
 	{
-		next = node->next;
-		node->n = next->n;
-		node->next = next->next;
-		free(next);
+		if (index == 0)
+			*head = NULL;
+		else
+			prev->next = NULL;
+		free(node);
 	}
 	else
 	{
+		if (index == 0)
+			*head = node->next;
+		else
+			prev->next = node->next;
 		free(node);
-		*head = NULL;
 	}
 
 	return (1);
