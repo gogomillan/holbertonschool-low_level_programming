2426eaa5b15b089b861ea7c2528ef523dc43e6d7
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-11-05 18:42:51 +0000

    Fix according to the requirements

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,7 +1,5 @@
 #include "lists.h"
 
-listint_t *swap_listint (listint_t *node, listint_t *prev);
-
 /**
  * reverse_listint - Reverse a listint_t list.
  * @head: Head of the list
@@ -10,37 +8,20 @@ listint_t *swap_listint (listint_t *node, listint_t *prev);
  */
 listint_t *reverse_listint(listint_t **head)
 {
-listint_t *new;
+listint_t *prev, *next;
 
 	if (head == NULL || *head == NULL)
 		return (NULL);
 
-	new = swap_listint ((*head)->next, *head);
-	(*head)->next = NULL;
-	*head = new;
+	prev = NULL;
+	while (*head)
+	{
+		next = (*head)->next; /* Saves the next position					*/
+		(*head)->next = prev; /* Makes point the current point to previous	*/
+		prev = *head;		  /* Sets the new previous from the current	pos	*/
+		*head = next;		  /* Moves the head to the next position		*/
+	}
+	*head = prev;			  /* The previous is the new head				*/
 
 	return (*head);
-}
-
-/**
- * swap_lisint - swap two elements of a listint_t list
- * @node: The current node
- * @prev: The previous node
- *
- * Return: Nothing
- */
-listint_t *swap_listint (listint_t *node, listint_t *prev)
-{
-listint_t *head;
-
-	if (node == NULL)
-		return (prev);
-	if (node->next == NULL)
-	{
-		node->next = prev;
-		return (node);
-	}
-	head = swap_listint (node->next, node);
-	node->next = prev;
-	return (head);
 }
