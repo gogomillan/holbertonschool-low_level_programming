f527db4a4c86105ecef752034f08e3daf49e3333
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-11-06 06:16:46 +0000

    Get index bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * get_bit - Returns the value of a specific bit
+ * @n: the unsigned long int value
+ * @index: the unsigned int position
+ *
+ * Return: Nothing
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned long int masc = 1;
+
+	n = (n >> index);
+	n = (n & masc);
+	return (n);
+}
