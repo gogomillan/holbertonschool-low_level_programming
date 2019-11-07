4a1a7041b40b8d39bb4194dbb1d9e9f0947f58c6
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-11-07 04:54:47 +0000

    Tread lightly, she is near, first commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: Name of the file
+ * @letters: Quantity of letters to print
+ *
+ * Return: The quantity of real letters printed.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int fd;				/* File Descriptor */
+char buf;			/* Buffer to store the readed data */
+ssize_t qty, c = 0;	/* Quantity readed */
+
+	if (filename == NULL)
+		return (0);
+	if (letters <= 0)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	do {
+		qty = read(fd, &buf, 1);
+		letters += qty;
+		c += qty;
+		write(1, &buf, qty);
+	} while (letters > 0 && qty > 0);
+
+	close(fd);
+	return (c);
+}
