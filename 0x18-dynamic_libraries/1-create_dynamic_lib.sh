fb1a774ea2a81a72c5f6304cbd4c490b97c799c5
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-12-13 22:26:42 +0000

    Fix the order of parameters .o (#1)

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 gcc -Wall -pedantic -Werror -Wextra -fPIC -c *.c
-gcc -Wall -pedantic -Werror -Wextra -shared *.o -o liball.so
+gcc -Wall -pedantic -Werror -Wextra -shared -o liball.so *.o
