7cc2921e64f2974e931c59f0b08c4a1e0471cf4b
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-01-16 15:05:10 +0000

    Fix betty style

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_delete - function that deletes a hash table.
+ * @ht: Table hash
+ *
+ * Return: Nothing
+ */
 void hash_table_delete(hash_table_t *ht)
 {
 hash_node_t *node, *tmp;
