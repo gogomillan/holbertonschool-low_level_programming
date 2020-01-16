1f50432583b4339c45417147646206a9aad2b56a
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-01-16 11:40:51 +0000

    djb2 (hash function)

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - generates a random number through a string given (hash)
+ * @str: string
+ *
+ * Return: hash number
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
