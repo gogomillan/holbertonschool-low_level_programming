d615412f6c1c15272b0b1e98487dc2fa98a7d0b7
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-01-16 15:01:35 +0000

    >>> del ht table hash

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+void hash_table_delete(hash_table_t *ht)
+{
+hash_node_t *node, *tmp;
+unsigned long int i;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			tmp = node;
+			node = node->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+		}
+	}
+	free(ht->array);
+	free(ht);
+	ht = NULL;
+}
