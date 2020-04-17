82be318ccc0995ac6a3336bf705fcbc200501619
Gnzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-04-16 19:56:35 -0500

    Linear search, commit!

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef GOGOMILLAN
+#define GOGOMILLAN
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif /* GOGOMILLAN */
